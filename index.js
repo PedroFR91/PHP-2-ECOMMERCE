e10428c9c7c3b2fd59a3ac9efb312e35bac79076
DevPed <pedromfr91@gmail.com>
2021-07-14 04:41:26 +0200

    Top sales

--- index.js
+++ index.js
@@ -4,4 +4,19 @@ $(document).ready(function () {
         dots: true,
         items: 1
     })
+    //top sale carousel
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: { items: 1 },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
 })
